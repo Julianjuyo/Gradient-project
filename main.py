d3b3a7173d4f5d6755740f93507d6656e70b7023
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2022-02-14 22:07:02 +0100

    Implementation of a graph final

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-
-from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-def PartialDerivativeX(x,y):
 
-    return ((2*x*y)-(y**2))
+def PartialDerivativeX(x, y):
+    return ((2 * x * y) - (y ** 2))
 
-def PartialDerivativeY(x,y):
-    return ((x**2)-(2*x*y))
 
-def calculateGradient(xCor,yCor):
+def PartialDerivativeY(x, y):
+    return ((x ** 2) - (2 * x * y))
+
+
+def calculateGradient(xCor, yCor):
     xCor = int(xCor)
     yCor = int(yCor)
-    vector = [1,2]
-    vector[0] = PartialDerivativeX(xCor,yCor)
+    vector = [1, 2]
+    vector[0] = PartialDerivativeX(xCor, yCor)
     vector[1] = PartialDerivativeY(xCor, yCor)
     return vector
 
-# function for z axea
+
 def f(x, y):
-    return ((x ** 2 *y) - (x * y ** 2 ))
+    return ((x ** 2 * y) - (x * y ** 2))
+
 
 if __name__ == '__main__':
 
@@ -28,25 +29,25 @@ if __name__ == '__main__':
     print("Write the coordinates of the plane.")
     print()
 
-    while(True):
+    while (True):
         print("Enter the numeric value for the x axis")
-        xCor = "4"# input()
+        xCor = "4"  # input()
         if xCor.isnumeric():
             break
         print("Not valid entrance")
 
-    while(True):
+    while (True):
         print("Enter the numeric value for the y axis")
-        yCor = "4"#input()
+        yCor = "4"  # input()
         if yCor.isnumeric():
             break
         print("Not valid entrance!!")
 
-    print("the coordinates in x,y are: (" + xCor + " , " + yCor+")")
+    print("the coordinates in x,y are: (" + xCor + " , " + yCor + ")")
 
-    ans = calculateGradient(xCor,yCor)
+    ans = calculateGradient(xCor, yCor)
     print()
-    print("The gradient at the coordinates (" + xCor + " , " + yCor+ ") is:")
+    print("The gradient at the coordinates (" + xCor + " , " + yCor + ") is:")
     print(ans)
 
     # x and y axis
